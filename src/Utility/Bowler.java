16adc0da62815254877af3d219daf9035605453b
Hitesh-yadav-18 <hitesh.yadav.18@mountblue.tech>
2021-11-17 11:48:59 +0530

    Added to handled  totalrun and ball count

---
+++ src/Utility/Bowler.java
@@ -0,0 +1,35 @@
+package Utility;
+
+public class Bowler {
+
+//    private String name;
+    private int totalRun;
+    private int balls;
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+    public int getTotalRun() {
+        return totalRun;
+    }
+
+    public void setTotalRun(int totalRun) {
+        this.totalRun = totalRun;
+    }
+
+    public int getBalls() {
+        return balls;
+    }
+
+    public void setBalls(int balls) {
+        this.balls = balls;
+    }
+
+
+
+}
