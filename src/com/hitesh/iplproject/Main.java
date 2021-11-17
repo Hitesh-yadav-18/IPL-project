bf5f9deb4ac41b0dca95e55e675f9049d04a94d8
Hitesh-yadav-18 <hitesh.yadav.18@mountblue.tech>
2021-11-17 19:34:43 +0530

    Added formattted getDelieveriesData() into Main

--- src/com/hitesh/iplproject/Main.java
+++ src/com/hitesh/iplproject/Main.java
@@ -1,4 +1,80 @@
 package com.hitesh.iplproject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
+
+    public static final int MATCH_ID = 0;
+    public static final int INNING = 1;
+    public static final int BATTING_TEAM = 2;
+    public static final int BOWLING_TEAM = 3;
+    public static final int OVER = 4;
+    public static final int BALL = 5;
+    public static final int BATSMAN = 6;
+    public static final int NON_STRIKER = 7;
+    public static final int BOWLER = 8;
+    public static final int IS_SUPER_OVER = 9;
+    public static final int WIDE_RUNS = 10;
+    public static final int BYE_RUNS = 11;
+    public static final int LEGBYE_RUNS = 12;
+    public static final int NO_BALL_RUNS = 13;
+    public static final int PANELTY_RUNS = 14;
+    public static final int BATSMAN_RUNS = 15;
+    public static final int EXTRA_RUNS = 16;
+    public static final int TOTAL_RUNS = 17;
+    public static final int PLAYER_DISMISSED = 18;
+    public static final int DISMISSAL_KIND = 19;
+    public static final int FIELDER = 20;
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static ArrayList<Delivery> getDeliveriesData(String fileName) throws IOException {
+        String line = "";
+        String[] columns = null;
+        ArrayList<Delivery> matches = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        bufferedReader.readLine();
+
+        while ((line = bufferedReader.readLine()) != null) {
+            columns = line.split(",");
+            Delivery del_obj = new Delivery();
+            del_obj.setMatchId(Integer.parseInt(columns[MATCH_ID]));
+            del_obj.setInning(Integer.parseInt(columns[INNING]));
+            del_obj.setBattingTeam(columns[BATTING_TEAM]);
+            del_obj.setBowlingTeam(columns[BOWLING_TEAM]);
+            del_obj.setOver(Integer.parseInt(columns[OVER]));
+            del_obj.setBall(Integer.parseInt(columns[BALL]));
+            del_obj.setBatsman(columns[BATSMAN]);
+            del_obj.setNonStriker(columns[NON_STRIKER]);
+            del_obj.setBowler(columns[BOWLER]);
+            del_obj.setIsSuperOver(Integer.parseInt(columns[IS_SUPER_OVER]));
+            del_obj.setWideRuns(Integer.parseInt(columns[WIDE_RUNS]));
+            del_obj.setByeRuns(Integer.parseInt(columns[BYE_RUNS]));
+            del_obj.setLegbyeRuns(Integer.parseInt(columns[LEGBYE_RUNS]));
+            del_obj.setNoballRuns(Integer.parseInt(columns[NO_BALL_RUNS]));
+            del_obj.setPaneltyRuns(Integer.parseInt(columns[PANELTY_RUNS]));
+            del_obj.setBatsmanRuns(Integer.parseInt(columns[BATSMAN_RUNS]));
+            del_obj.setExtraRuns(Integer.parseInt(columns[EXTRA_RUNS]));
+            del_obj.setTotalRuns(Integer.parseInt(columns[TOTAL_RUNS]));
+            if (columns.length > 18)
+                del_obj.setPlayerDismissed(columns[PLAYER_DISMISSED]);
+            if (columns.length > 19)
+                del_obj.setDismissalKind(columns[DISMISSAL_KIND]);
+            if (columns.length > 20)
+                del_obj.setFielder(columns[FIELDER]);
+
+            matches.add(del_obj);
+        }
+        bufferedReader.close();
+        return matches;
+    }
+
 }
